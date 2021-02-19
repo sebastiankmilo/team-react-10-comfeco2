047973c86ccd299acc41f6f995e9d476d92213fc
Sebastian camilo cruz dussan <sebastiankmilo1996@gmail.com>
2021-02-18 22:30:22 -0500

    la propiedad name de inputText se puso como requeridad en las proptypes

--- src/components/screens/auth/RegisterFrom.js
+++ src/components/screens/auth/RegisterFrom.js
@@ -1,21 +1,152 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
-import { Form } from 'reactstrap'
+import {
+  Card,
+  Button,
+  CardImg,
+  CardBody,
+  Form,
+  CardTitle,
+  FormGroup,
+  Col,
+} from 'reactstrap'
+import { Link } from 'react-router-dom'
+
 import { InputText } from '../../ui/inputs/InputText'
 
+import { RouteMap } from '../../../constants/RouteMap'
+import { useAuthReducer } from '../../../hooks'
+import { Loader } from '../../ui/common/Loader'
+import * as AuthAction from '../../../actions/AuthAction'
+
+/**
+ * Asset
+ */
+import Logo from '../../../assets/img/logo.svg'
+import Password from '../../../assets/img/password.svg'
+import Email from '../../../assets/img/email.svg'
+import Icon from '../../../assets/img/icon.svg'
 export const RegisterFrom = () => {
-  const { register, errors } = useForm()
+  const [user, setUser] = useState({ email: '', password: '' })
+  const [{ isRequesting, error, message }, dispatch] = useAuthReducer()
+  const { register, errors, handleSubmit } = useForm()
+
+  const handleChange = ({ name, value }) => {
+    setUser({ ...user, [name]: value })
+  }
+
+  const onSubmit = () => {
+    const { email, password } = user
+    dispatch(AuthAction.login(email, password))
+  }
+
+  useEffect(() => {
+    if (error) console.log(message)
+  }, [error, message])
+
+  const { nombre, email, password, passwordConfirm } = user
+
   return (
-    <Form>
-      <InputText
-        id="nickname"
-        name="nickname"
-        placeholder="Nombre"
-        errors={errors}
-        innerRef={register({
-          required: true,
-        })}
-      />
-    </Form>
+    <>
+      <Card className="border-0" style={{ width: '30rem' }}>
+        <CardImg
+          top
+          src={Logo}
+          alt="logo-comfeco"
+          height="90px"
+          className="mt-3"
+        />
+        <CardBody>
+          <CardTitle
+            className="text-center font-size-34 color-purple-text mt-4"
+            style={{
+              fontWeight: '500',
+              color: '#756585',
+            }}
+            tag="h1"
+          >
+            Registrarse
+          </CardTitle>
+          {isRequesting ? (
+            <Loader />
+          ) : (
+            <Form onSubmit={handleSubmit(onSubmit)}>
+              <FormGroup className="mt-5 position-relative ">
+                <InputText
+                  id="nombre"
+                  name="nombre"
+                  icon={Icon}
+                  type="nombre"
+                  value={nombre}
+                  placeholder="nombre"
+                  onChange={handleChange}
+                  errors={errors}
+                  innerRef={register({
+                    required: 'Campo obligatorio',
+                  })}
+                  className="inputText"
+                />
+                <InputText
+                  id="email"
+                  name="email"
+                  icon={Email}
+                  value={email}
+                  placeholder="Correo Electrónico"
+                  onChange={handleChange}
+                  errors={errors}
+                  innerRef={register({
+                    required: 'Campo obligatorio',
+                    pattern: {
+                      value: /[A-Za-z0-9._%+-]{3,}@[a-zA-Z]{3,}([.]{1}[a-zA-Z]{2,}|[.]{1}[a-zA-Z]{2,}[.]{1}[a-zA-Z]{2,})/i,
+                      message: 'El Email debe ser valido',
+                    },
+                  })}
+                  className="inputText"
+                />
+                <InputText
+                  id="password"
+                  name="password"
+                  icon={Password}
+                  type="password"
+                  value={password}
+                  placeholder="Contraseña"
+                  onChange={handleChange}
+                  errors={errors}
+                  innerRef={register({
+                    required: 'Campo obligatorio',
+                  })}
+                  className="inputText"
+                />
+                <InputText
+                  id="password"
+                  name="password"
+                  icon={Password}
+                  type="password"
+                  value={password}
+                  placeholder="Confirmar Contraseña"
+                  onChange={handleChange}
+                  errors={errors}
+                  innerRef={register({
+                    required: 'Campo obligatorio',
+                  })}
+                  className="inputText"
+                />
+              </FormGroup>
+
+              <Col className="col text-center">
+                <Button
+                  outline
+                  color="primary"
+                  type="submit"
+                  className="rounded-pill font-size-14 mt-5 inputButton mb-4"
+                >
+                  Iniciar sesión
+                </Button>
+              </Col>
+            </Form>
+          )}
+        </CardBody>
+      </Card>
+    </>
   )
 }
